35dd13997087fc6c26c0eb4fa7b0928b288f8b2c
HashirMughal <86366991+HashirMughal@users.noreply.github.com>
2021-07-08 18:16:44 +0500

    Add files via upload

---
+++ 16th-020721/If-else-switch.js
@@ -0,0 +1,29 @@
+        //let role='admin';
+        //switch(role){
+            //   case 'guest':
+        //         console.log('guest user');
+        //         break;
+        //     case 'admin':
+        //         console.log('admin user');
+        //         break;
+        //     default:
+        //         console.log('unknown user');
+        // }
+        // let role ='admin';
+        // if(role == 'guest'){
+        //     console.log('Guest user');
+        // }
+        // else if(role == 'admin' ){
+        //     console.log('Admin user');
+        // }
+        // else('unkown user')
+        let role ='guest';
+        if(role == 'guest'){
+            console.log('Guest user');
+        }
+        else if(role == 'admin' ){
+            console.log('Admin user');
+        }
+        else{
+            console.log('unknown user');
+        }
---
+++ 16th-020721/switch-if_else.js
@@ -0,0 +1,8 @@
+let hours = 13;
+if(hours >= 6 && hours <= 12){
+    console.log('GOOD MORNING');
+}
+else if(hours >=12 && hours <= 18){
+    console.log('GOOD AFTERNOON');
+}
+else('GOOD EVENING')
