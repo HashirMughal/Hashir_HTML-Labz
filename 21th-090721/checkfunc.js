0e22fc34c36579a86fce89000b5bcec14706efc1
HashirMughal <86366991+HashirMughal@users.noreply.github.com>
2021-07-11 14:28:55 +0500

    Add files via upload

---
+++ 21th-090721/checkfunc.js
@@ -0,0 +1,5 @@
+function check(width,height){
+    return (width>height) ? 'Landscape' : 'Portraite';
+}
+let values =check(3300,500);
+console.log(values);
---
+++ 21th-090721/greaterfunc.js
@@ -0,0 +1,7 @@
+function max(a,b){
+    if(a>b) return a;
+    else return b;
+}
+let number =max(2,5);
+    console.log(number);
+///////
