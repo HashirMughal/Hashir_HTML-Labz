01902d600c37d5f41a8b2229afdfdccefe2c2a95
HashirMughal <86366991+HashirMughal@users.noreply.github.com>
2021-07-12 10:02:25 +0500

    Update FIZZFUZZ.js

--- Assignments/FIZZFUZZ.js
+++ Assignments/FIZZFUZZ.js
@@ -3,7 +3,7 @@
 //divisible by 5 = buzz
 //divisible by both = fizzbuzz
 //not divisible by both = input
-let 1=5;
+let i=5;
 if(i % 3 == 0 && i % 5 == 0){
     console.log(i,'FIZZBUZZ');
 }else if(i % 3 == 0){
