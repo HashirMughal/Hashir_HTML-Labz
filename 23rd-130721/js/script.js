39399a04f5124b341d0c72e410d9bdfa06a61ae8
HashirMughal <86366991+HashirMughal@users.noreply.github.com>
2021-07-13 16:11:48 +0500

    Add files via upload

---
+++ 23rd-130721/js/script.js
@@ -0,0 +1,10 @@
+$(function(){
+    $(".first-box").fadeOut(2000);
+    $(".second-box").fadeOut(4000);
+    $(".third-box").fadeOut(6000);
+    $(".third-box").fadeToggle(6000);
+    // $(".third-box").fadeOut(slow);
+    // $(".third-box").fadeOut(fast);
+    $(".first-box").fadeIn(8000);
+    $(".second-box").fadeIn(8000);
+});
